4b3c344b178b4f593b8595fc48c574858a32afca
calopsitapreta <139503167+Luanfslv@users.noreply.github.com>
2024-01-19 10:45:37 -0300

    att

---
+++ main.js
@@ -0,0 +1,8 @@
+let listaDeItens = []
+
+const form = document.getElementById("form-itens")
+const itensInput = document.getElementById("receber-item")
+
+form.addEventListener("submit", function(evento){
+    evento.preventDefault()
+})
